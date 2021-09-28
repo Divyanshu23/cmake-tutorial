115c1969c4a5a0017be4b1ad527a2a3a0745ad30
Divyanshu Gangwar <dvyanshu@iitk.ac.in>
2021-09-28 17:02:07 +0530

    Intial Commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<adder.hpp>
+using namespace std;
+
+
+float add(float a, float b);
+
+int main()
+{
+    cout<<"Hello World\n";
+
+    cout<<add(23.4, 16.9)<<"\n";
+    return 0;
+}
