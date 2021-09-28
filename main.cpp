a05d3489708d919a9a3ebd107b462fcfff01600f
Divyanshu Gangwar <dvyanshu@iitk.ac.in>
2021-09-29 00:48:59 +0530

    Integrate glfw submodule in main code.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
-#include<iostream>
+#include<bits/stdc++.h>
 #include<adder.hpp>
+#include<GLFW/glfw3.h>
+
 using namespace std;
 
 
@@ -8,7 +10,29 @@ float add(float a, float b);
 int main()
 {
     cout<<"Hello World\n";
-
     cout<<add(23.4, 16.9)<<"\n";
+
+    GLFWwindow* window;
+
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
     return 0;
 }
